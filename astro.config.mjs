cb700a97c8a4b22487173ba1884edc4558548a81
Jay <jaydrum1991@gmail.com>
2026-07-11 20:28:11 +0100

    Add custom domain: darkstyle.media

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://jayd1991.github.io',
-  base: '/darkstylemedia',
+  site: 'https://darkstyle.media',
   output: 'static',
 });
